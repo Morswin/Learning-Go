10bc5c8d6182ad1b67bbef430ca4310266fdb4f0
Morswin <41349347+Morswin@users.noreply.github.com>
2025-06-18 14:07:28 +0200

    Add Task component

---
+++ frontend/components/Task.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { ref, defineProps } from 'vue';
+
+    interface Props {
+        title: string,
+        description: string
+    }
+
+    const props = defineProps<Props>();
+</script>
+
+<template>
+    <fieldset>
+        <legend>
+            <h3>{{ props.title }}</h3>
+        </legend>
+        <p>{{ props.description }}</p>
+        <button>Finish</button>
+        <button>Cancel</button>
+    </fieldset>
+</template>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+</script>
+
+<template>
+    <article>
+        <!-- Here goes the main stuff -->
+        <h1>TODO App</h1>
+        <Task title="Title 1" description="A nice description"></Task>
+        <Task title="Title 2" description="A fitting description"></Task>
+        <Task title="Title 3" description="A worthy description"></Task>
+    </article>
+    <nav>
+        <!-- Here go the nav inputs -->
+    </nav>
+</template>
